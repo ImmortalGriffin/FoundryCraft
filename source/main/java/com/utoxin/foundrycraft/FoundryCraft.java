716565a0dd00ec39e2733f75947caf2a6d6b4dec
Matthew Walker <mwalker@kydance.net>
2014-06-24 08:30:23 -0600

    Getting started

---
+++ source/main/java/com/utoxin/foundrycraft/FoundryCraft.java
@@ -0,0 +1,27 @@
+package com.utoxin.foundrycraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="FoundryCraft", name="Foundry Craft", version="1.7.2-0.1")
+public class FoundryCraft {
+    @Mod.Instance("FoundryCraft")
+    public static FoundryCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
